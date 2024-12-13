a0481f2cafd3deabf596836e0b383c1cb9763f41
Security Researcher <dr.irfanblt@gmail.com>
2024-12-13 14:03:28 +0100

    Update rePacker.py
    + added refactoring
    + added __name__ == "__main__"

--- rePacker/rePacker.py
+++ rePacker/rePacker.py
@@ -1,19 +1,13 @@
-import os
+import os, time
 import subprocess
 import shutil
 
-# Define paths
-input_folder = "/input/benign_exe/test/"  # Folder containing files to be packed
-output_folder = "/output/benign_exe/packed_files/"  # Base folder for packed files
 packers = {
     "UPX": "upx",
-    "UPX LZMA": "upx_lzma"
+    "UPX_LZMA": "upx_lzma"
     #"PyInstaller": "pyinstaller",
     #"Nuitka": "nuitka"
 }
-
-# Ensure output folder exists
-os.makedirs(output_folder, exist_ok=True)
 
 def pack_with_upx(file_path, output_dir):
     packed_file_path = os.path.join(output_dir, os.path.basename(file_path))
@@ -39,25 +33,43 @@ def pack_with_mpress(file_path, output_dir):
 
 packer_functions = {
     "UPX": pack_with_upx,
-    "UPX LZMA": pack_with_upx_lzma,
+    "UPX_LZMA": pack_with_upx_lzma,
     "PyInstaller": pack_with_pyinstaller,
     "Nuitka": pack_with_nuitka
 }
 
-# Process files for each packer
-for packer, command in packers.items():
-    print(f"Processing with {packer}...")
-    packer_output_dir = os.path.join(output_folder, packer)
-    os.makedirs(packer_output_dir, exist_ok=True)
+def packer_process(input_folder, output_folder):
+    # Process files for each packer
+    for packer, command in packers.items():
+        print(f"Processing with {packer}...")
+        packer_output_dir = os.path.join(output_folder, packer)
+        os.makedirs(packer_output_dir, exist_ok=True)
 
-    for file in os.listdir(input_folder):
-        file_path = os.path.join(input_folder, file)
+        for file in os.listdir(input_folder):
+            file_path = os.path.join(input_folder, file)
 
-        if os.path.isfile(file_path):
-            print(f"Packing {file} using {packer}...")
-            try:
-                packer_functions[packer](file_path, packer_output_dir)
-            except Exception as e:
-                print(f"Error packing {file} with {packer}: {e}")
+            if os.path.isfile(file_path):
+                print(f"Packing {file} using {packer}...")
+                try:
+                    packer_functions[packer](file_path, packer_output_dir)
+                except Exception as e:
+                    print(f"Error packing {file} with {packer}: {e}")
+    print("Packing completed!")
 
-print("Packing completed!")
+def main(in_dir, out_dir):
+    packer_process(in_dir, out_dir)
+    
+if __name__ == "__main__":
+    print("[" + __file__ + "]'s last modified: %s" % time.ctime(os.path.getmtime(__file__)))
+    # Define paths
+    in_dir = "/input/benign_exe/test/"  # Folder containing files to be packed
+    out_dir = "/output/benign_exe/packed_files/"  # Base folder for packed files
+    if not os.path.exists(in_dir):
+        print(f"Directory: '{in_dir}' does not exist.")
+        exit()         
+    print(f"\n\nBINARY Directory:\t\t{in_dir}")
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
+    print(f"Packed BINARY Files will save:\t{out_dir}")   
+    main(in_dir, out_dir)
